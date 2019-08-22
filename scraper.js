3d82e1cb3ded1ac3c107dee6e3a8d3e42c6ba444
mchap <mary.chapman@generalassemb.ly>
2019-08-22 00:06:53 -0400

    prints out only S-1s

--- scraper.js
+++ scraper.js
@@ -1,11 +1,35 @@
+
+const http = require('http')
+const fs = require('fs');
+
 const cheerio = require("cheerio");
 const request = require("request");
+const axios = require('axios')
 
-request("https://www.sec.gov/Archives/edgar/full-index/2019/QTR3/", (error, response, html) => {
-    if (!error && response.statusCode === 200) {
-        const $ = cheerio.load(html);
-        const table = $("[summary=heding]");
+// const file = fs.createWriteStream("file2.idx");
+// const req = http.get("http://www.sec.gov/Archives/edgar/full-index/2019/QTR3/master.idx", function(response) {
+//   response.pipe(file);
+//   file.on('finish', function() {
+//     file.close(cb);
+//   });
+// });
 
-        console.log(table.html());
-    }
-});
+// const file = fs.createWriteStream("file.idx");
+// const req = http.get("http://www.sec.gov/Archives/edgar/full-index/2019/QTR3/master.idx", function(response) {
+//     console.log(response);  
+//     response.pipe(file);
+// });
+
+// const file = fs.createWriteStream("archive.idx");
+axios.get('http://www.sec.gov/Archives/edgar/full-index/2019/QTR3/master.idx').then((res) => {
+    //console.log(Object.keys(res))  
+    const arr = res.data.split('\n')
+    const items = arr.map(item => item.split('|'))
+    const s1Docs = items.filter(item => item[2] === "S-1")
+
+    console.log(s1Docs)
+        
+})
+
+
+http.createServer().listen(8080); //the server object listens on port 8080
