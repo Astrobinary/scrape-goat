4b6e38313ab83c93a84d0da579ce9a64227db21f
Brandon <brandilla1337@gmail.com>
2019-08-22 22:40:13 -0400

    Added some comments

--- scraper.js
+++ scraper.js
@@ -8,8 +8,8 @@ const sampleJob = ["SmileDirectClub, Inc.", "S-1", "1775625", "2019-08-16", "htt
 
 const fetchOnlyS1 = (year, quarter) => {
     axios.get(`https://www.sec.gov/Archives/edgar/full-index/${year}/QTR${quarter}/crawler.idx`).then(res => {
-        const cleanList = res.data.replace(/[\s\S]*?---?\n/, "").replace(/ {2,}/gm, "  ");
-        const arr = cleanList.split("\n").map(item => item.trim().split("  "));
+        const cleanList = res.data.replace(/[\s\S]*?---?\n/, "").replace(/ {2,}/gm, "  "); //Removes the top header info and normalize spaces for split.
+        const arr = cleanList.split("\n").map(item => item.trim().split("  ")); //Splits on every 2 spaces
         const _s1 = arr.filter(item => item[1] === "S-1");
 
         return _s1;
@@ -22,10 +22,11 @@ const scrapeData = job => {
 
         const accessionNum = $("#secNum").text().trim().slice(18);
 
-        const mailer = $(".mailer").text().trim().split('\n');
+        const mailer = $(".mailer").text().trim().split('\n'); //Grab mailer block
         const address = `${mailer[1].trim()}, ${mailer[3]}`
         const phoneNum = formatPhoneNumber(mailer[8])
 
+        //Sample output
         console.log(`
         Accession No: ${accessionNum}
         Address: ${address}
@@ -38,13 +39,12 @@ const scrapeData = job => {
     });
 };
 
-
+//Simple number formatter 
 const formatPhoneNumber = (phoneNumberString) => {
-    var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
-    var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
-    if (match) {
-        return '(' + match[1] + ') ' + match[2] + '-' + match[3]
-    }
+    const cleaned = ('' + phoneNumberString).replace(/\D/g, '')
+    const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+    if (match) return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+
     return null
 }
 
